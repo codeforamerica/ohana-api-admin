daa75514f49c62bf8965821ed73acf1cd6a42f2d
Moncef Belyamani <giter@done>
2013-09-04 16:02:42 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hsa::Application.config.secret_token = '7f5df2af31826553ca56e75c2890e802165249fc528a0c343fad14c604750be43c073ec41bad68e935d64f26a878d48a379d649e4ba2f7ff87fc08826924fce3'
