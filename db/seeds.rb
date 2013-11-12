aa8d0993b346118b8e982f401b42ec675b0a22b2
Moncef Belyamani <giter@done>
2013-11-11 19:14:31 -0800

    Configure mongoid.yml and newrelic.yml for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,3 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
-
-puts '===> Setting up first test user...'
-user = User.create! :name => 'First User',
-                    :email => 'moncef@legalaidsmc.org',
-                    :password => 'mong01dtest',
-                    :password_confirmation => 'mong01dtest'
-user.confirm!
-
-puts '===> Setting up second test user...'
-user2 = User.create! :name => 'Second User',
-                     :email => 'moncef@ivsn.org',
-                     :password => 'mong01dtest',
-                     :password_confirmation => 'mong01dtest'
-user2.confirm!
-
-puts '===> Setting up admin user...'
-user3 = User.create! :name => 'Admin User',
-                     :email => 'moncef@codeforamerica.org',
-                     :password => 'mong01dtest',
-                     :password_confirmation => 'mong01dtest'
-user3.confirm!
