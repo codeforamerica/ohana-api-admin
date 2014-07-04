09dd4f2f0957dfb7f388d29a3250653bb89d5204
Moncef Belyamani <giter@done>
2014-07-04 15:21:12 -0400

    Remove sensitive info from logs

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ module Hsa
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation, :api_token]
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
